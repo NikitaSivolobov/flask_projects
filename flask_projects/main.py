ee9f60fd39ff70c90724ef557e5afb000abbaced
nikita1314 <nikita1314@yandex.ru>
2022-05-25 20:34:37 +0300

    Flask project

---
+++ flask_projects/main.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index/")
+@app.route("/index")
+def index():
+    return '''
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Home page</title>
+                <meta charset="utf-8">
+            </head>
+
+            <body>
+                <p>Hello, Flask!</p>
+            </body>
+
+        </html>
+
+        <script>
+            alert('Wow')
+        </script>
+        '''
+if __name__ == '__main__':
+    app.run()
