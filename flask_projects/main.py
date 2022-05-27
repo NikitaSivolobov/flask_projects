4cbf3e821c797197f212aae9228d4f642f7dec3e
nikita1314 <nikita1314@yandex.ru>
2022-05-27 21:18:41 +0300

    Добавлена возможность выбора случайной переменной

--- flask_projects/main.py
+++ flask_projects/main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from random import choice
+random_name = choice(['Dasha', 'Elize', 'Rita', 'Nikita'])
 
 app = Flask(__name__)
 
@@ -7,8 +9,8 @@ app = Flask(__name__)
 @app.route("/index")
 def index():
     return render_template('index.html',
-                           name='Никита',
-                           title='Home page',
+                           name=random_name,
+                           title='Home Page',
                            project_number=100)
 
 
